2b4a853086a5c473448d6922c30ddc87825ac4d8
ciaojhen <ciao2985@gmail.com>
2018-04-07 11:47:46 +0800

    hw4

---
+++ 103302003HW4.py
@@ -0,0 +1,48 @@
+# Pascal's triangle
+
+import sys, getopt
+
+# input args
+opts, args = getopt.getopt(sys.argv[1:], "no")
+
+#flag -n:True -o:False
+flag = True
+
+for op ,value in opts:
+    if op == "-n":
+        flag = True
+    elif op == "-o":
+        flag = False
+    else:
+        print('unknown options')
+        exit()
+
+def pascal_triangle(level):
+    if level == 0:
+        return [1]
+    else:
+        output = list(range(level+ 1))
+        for i in range(0,level+1):
+            if i == 0 or i == len(pascal_triangle(level-1)):
+                output[i] = 1
+            else:
+                output[i] = pascal_triangle(level-1)[i] + pascal_triangle(level-1)[i-1]
+        return output
+
+# test input number
+if len(args) == 1:
+    if args[0].isdigit():
+        if int(args[0]) >= 0:
+            level = int(args[0])
+        else:
+            print('Please input positive number')
+            exit()
+else:
+    print('Please input level number again')
+    exit()
+    
+if flag: # -n
+    for i in range(0,level+1):
+        print(pascal_triangle(i))
+else: # -o
+    print(pascal_triangle(level))
