037fcd4bd668d0d6e0ded6285aa8ca6429c2e113
ciaojhen <ciao2985@gmail.com>
2018-04-24 11:58:15 +0800

    complete graph

--- 103302003HW6.py
+++ 103302003HW6.py
@@ -8,38 +8,52 @@ listG = [[[1, 1, 3.5], [2, 1, 2.8], [3, 2, 1.5]],
          [[10, 5, 1.4], [11, 2, 2.3]]]
 
 def dfs_iterative(adjLists, start, final):
-    stack = []
-    stack.append(start)
-    n = len(adjLists)
-    visited = []
-    for i in range(0,n):
-        visited.append(False)
-         
-    while(len(stack)>0):
-        v = stack.pop()
-        if (v == final):
-            break
-        if(not visited[v]):
-            visited[v] = True
-            print('v = ', v, " ")
-            stack_aux = []
-            listnode = []
-            for node in adjLists[v]:
-                listnode.append(node[1])
+    count = 0
+    roadList = []
+    while(count < 3):
+        ActualRoadList = []
+        roadName = 0
+        length = 0
+        stack = []
+        stack.append(start)
+        visited = []
+        for i in range(0, 7):
+            visited.append(False)
+        isRepeat = False
+        while(len(stack)>0):
+            v = stack.pop()
+            if (v == final):
+                break
+            if(not visited[v]):
+                visited[v] = True
+                listnode = []
+                for node in adjLists[v]:
+                    listnode.append(node)
+                flagAdd = True
+                while flagAdd:
+                    tmp = choice(listnode)
+                    if(not visited[tmp[1]]):
+                        ActualRoadList.append(tmp[0])
+                        length += tmp[2]
+                        roadName += tmp[0]
+                        
+                        stack.append(tmp[1])
+                        flagAdd = False
                 
-            #print('listnode:', listnode)
-            flagAdd = True
-            while flagAdd:
-                tmp = choice(listnode)
-                #print('tmp = ', tmp)
-                
-                if(not visited[tmp]):
-                    stack_aux.append(tmp)
-                    #print('stack aux: ', stack_aux)
-                    stack.append(stack_aux.pop())
-                    flagAdd = False
-            #while(len(stack_aux)>0):
-                #stack.append(stack_aux.pop())
-            print(stack)
+        for road in roadList:
+            if(road == roadName):
+                isRepeat = True
+                break
+        
+        if(not isRepeat):
+            roadList.append(roadName)
+            count += 1
+            print('')
+            print('Road :', ActualRoadList)
+            print('total length:', "%.1f" %length)
 
-dfs_iterative(listG, 0, 3)
+start = input('Please input start point:(0~6):')
+final = input('Please input final point:(0~6):')
+s = int(start)
+f = int(final)
+dfs_iterative(listG, s, f)
